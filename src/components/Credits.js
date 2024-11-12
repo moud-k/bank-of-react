4f0732957c9f00ecdfb56454152eb5c2fa967280
Mahmoud Kourani <mahmoudkourani@Mahmouds-MBP.lan>
2024-11-12 00:35:13 -0500

    Updated App.js to store the fetched data of credits and debits into the proper variables; updated credits.js to display each credit and its information

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -7,13 +7,30 @@ Note: You need to work on this file for the Assignment.
 import {Link} from 'react-router-dom';
 
 const Credits = (props) => {
-  return (
-    <div>
-      <h1>Credits</h1>
-      <br/>
-      <Link to="/">Return to Home</Link>
-    </div>
-  );
+
+    let creditsView = () => {
+        const { credits } = props;
+        return credits.map((credit) => {  // Extract "id", "amount", "description" and "date" properties of each credits JSON array element
+          let date = credit.date.slice(0,10);
+          return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+        });
+      }
+
+      return (
+        <div>
+          <h1>Credits</h1>
+    
+          {creditsView()}
+    
+          <form onSubmit={props.addCredit}>
+            <input type="text" name="description" />
+            <input type="number" name="amount" />
+            <button type="submit">Add Credit</button>
+          </form>
+          <br/>
+          <Link to="/">Return to Home</Link>
+        </div>
+      );
 }
 
 export default Credits;
